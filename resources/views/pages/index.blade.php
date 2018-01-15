b69d84f58e3a1f2ef95fb563fc7352118921109d
brianokinyi <brianokinyi.bo@gmail.com>
2018-01-15 22:53:33 +0300

    Working with blade syntax

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the Laravel application</p>
+    <div class="jumbotron text-center">
+        <h1>{{$title}}</h1>
+        <p>This is the Laravel application from the Laravle From Scratch YouTube Series</p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+        </p>
+    </div>
 @endsection
 
