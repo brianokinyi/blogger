934f205c4ca2b2d481833d062c484e9af9df319a
brianokinyi <brianokinyi.bo@gmail.com>
2018-01-18 12:01:32 +0300

    added ck editor, create post form, save changes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,7 +20,7 @@ class PostsController extends Controller
         //$posts= Post::where('title', 'Post Two')->get();
         //$posts = DB::select("SELECT * FROM posts");
         //$posts =  Post::all();
-        $posts = Post::orderBy('title', 'desc')->paginate(1);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
         return view('posts.index')->with('posts', $posts);
     }
 
@@ -31,7 +31,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -42,7 +42,18 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate ($request, [
+            'title' => 'required', 
+            'body' => 'required'
+        ]);
+        //Create post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+        return redirect('/posts/')->with('success', 'post created');
     }
 
     /**
